645a1e2ca4b6322fcfe937d8ac42fa32ebdcc62e
janbrederecke <j.brederecke@uke.de>
2021-08-24 16:26:13 +0200

    First work on the layout done...

--- hf-score-shiny-app/app.R
+++ hf-score-shiny-app/app.R
@@ -8,107 +8,141 @@ load(file = paste0(here::here(), "/hf-score-shiny-app/rsf_primary.RData"))
 
 # Define UI for application
 ui <- fluidPage(
-    # Application title
-    titlePanel("Biomarcare Heart Failure Score Calculator"),
-    
-    # Sidebar with inputs for the predictor variables
-    sidebarLayout(
+        # Application title
+        titlePanel("Biomarcare Heart Failure Score Calculator"),
+        
+        
         sidebarPanel(
-            # Inputs:
-            numericInput(
-                inputId = "age1",
-                label = "Age in years",
-                value = 50,
-                min = 50,
-                max = 100,
-                step = 1
-            ),
+            img(src = "biomarcare.png", width = 150),
             hr(),
-            selectInput(
-                inputId = "male",
-                label = "Sex",
-                choices = c(0, 1),
-                selected = 0
-            ),
-            hr(),
-            numericInput(
-                inputId = "bmi",
-                label = "Body Mass Index",
-                value = 25,
-                min = 15,
-                max = 60,
-                step = 0.1
-            ),
-            hr(),
-            selectInput(
-                inputId = "basediab1",
-                label = "Diabetes",
-                choices = c(0, 1),
-                selected = 0
-            ),
-            hr(),
-            selectInput(
-                inputId = "drug_hypert",
-                label = "Hypertension Medication",
-                choices = c(0, 1),
-                selected = 0
-            ),
-            hr(),
-            selectInput(
-                inputId = "dsmoker",
-                label = "Daily Smoker",
-                choices = c(0, 1),
-                selected = 0
-            ),
-            hr(),
-            selectInput(
-                inputId = "basemi2",
-                label = "Myocardial Infarction in the past",
-                choices = c(0, 1),
-                selected = 0
-            ),
-            hr(),
-            numericInput(
-                inputId = "alcave",
-                label = "Alcohol consumption",
-                value = 0,
-                min = 0,
-                max = 40,
-                step = 1
-            ),
-            hr(),
-            numericInput(
-                inputId = "systm",
-                label = "Systolic Blood Pressure",
-                value = 80,
-                min = 50,
-                max = 250,
-                step = 1
-            ),
-            hr(),
-            numericInput(
-                inputId = "nt_pro_bnp",
-                label = "Levels of NT-proBNP",
-                value = 25,
-                min = 0,
-                max = 100,
-                step = 0.1
-            ),
+            actionButton("enter", label = "Calculate Risk"),
+            width = 2,
             hr(),
             
+            # Output
+            textOutput("prediction"),
+            hr(),
+            ),
+        
+        # Main panel
+        mainPanel(
+            # Info text
+            p("The Biomarker for Cardiovascular Risk Assessment across Europe (BiomarCaRE) consortium is an EU-funded consortium including over 30 partner from academia and industry. BiomarCaRE aims to determine the value of established and emerging biomarkers to improve risk estimation of cardiovascular disease in Europe. BiomarCaRE relies on an exceptional resource of large scale epidemiological cohorts with long term follow-up and available bio specimens based on the population of the MORGAM Project as well as several cardiovascular disease cohorts and clinical trials. Uniquely, all epidemiological and clinical phenotypes as well as disease outcomes have been harmonized in a joint data base. Within BiomarCaRE, an interdisciplinary team with various expertise, the value of established and emerging biomarkers is determined across the cohorts using innovative approaches and technologies.")
+            ),
+        hr(),
             
-            actionButton("enter", label = "Enter Values"),
-            width = 2
-            
+        
+        # Enter information below - statement
+        fluidRow(
+            column(10,
+                   p("Enter your information below:"),
+                   hr()
+                   )
         ),
         
-        # Main panel for displaying outputs
-        mainPanel(
-            titlePanel("Here goes the output"),
-            textOutput("prediction")
-            
+        # Inputs for the predictor variables
+        fluidRow(
+            column(2,
+                wellPanel(
+                    numericInput(
+                        inputId = "age1",
+                        label = "Age in years",
+                        value = 50,
+                        min = 50,
+                        max = 100,
+                        step = 1
+                    ),
+                    hr(),
+                    selectInput(
+                        inputId = "male",
+                        label = "Sex",
+                        choices = c(0, 1),
+                        selected = 0
+                    ),
+                    hr(),
+                )
+            ),
+            column(2,
+                wellPanel(
+                    numericInput(
+                        inputId = "bmi",
+                        label = "Body Mass Index",
+                        value = 25,
+                        min = 15,
+                        max = 60,
+                        step = 0.1
+                    ),
+                    hr(),
+                    selectInput(
+                        inputId = "basediab1",
+                        label = "Diabetes",
+                        choices = c(0, 1),
+                        selected = 0
+                    ),
+                    hr(),
+                )
+            ),
+            column(2,
+                wellPanel(
+                    selectInput(
+                        inputId = "drug_hypert",
+                        label = "Hypertension Medication",
+                        choices = c(0, 1),
+                        selected = 0
+                    ),
+                    hr(),
+                    selectInput(
+                        inputId = "dsmoker",
+                        label = "Daily Smoker",
+                        choices = c(0, 1),
+                        selected = 0
+                    ),
+                    hr(),
+                )
+            ),
+            column(2,
+                wellPanel(
+                    selectInput(
+                        inputId = "basemi2",
+                        label = "Myocardial Infarction in the past",
+                        choices = c(0, 1),
+                        selected = 0
+                    ),
+                    hr(),
+                    numericInput(
+                        inputId = "alcave",
+                        label = "Alcohol consumption",
+                        value = 0,
+                        min = 0,
+                        max = 40,
+                        step = 1
+                    ),
+                    hr(),
+                )
+            ),
+            column(2,
+                wellPanel(
+                    numericInput(
+                        inputId = "systm",
+                        label = "Systolic Blood Pressure",
+                        value = 80,
+                        min = 50,
+                        max = 250,
+                        step = 1
+                    ),
+                    hr(),
+                    numericInput(
+                        inputId = "nt_pro_bnp",
+                        label = "Levels of NT-proBNP",
+                        value = 25,
+                        min = 0,
+                        max = 100,
+                        step = 0.1
+                    ),
+                    hr()
+                )
+            )
         )
-    )
 )
 
 # Define server logic required to predict from the rsf model
@@ -135,13 +169,10 @@ server <- function(input, output, session) {
         pred <- predict(rsf_model, data)
         pred_matrix <- pred[["survival"]]
         v <- which(pred[["unique.death.times"]] >= (365.25*5))
-
         paste0("Your estimated risk for HF within 5 years is: ",
         round((1 - pred_matrix[v[1]]) * 100, 2), "%."
         )
     })
-    
-    
     
 }
 
