6a3f0993dca6457f1f7196ccbcf74a866c756bcb
janbrederecke <j.brederecke@uke.de>
2022-03-14 15:19:27 +0100

    Updated the extended model

--- hf-score-shiny-app/bm_score_ext.R
+++ hf-score-shiny-app/bm_score_ext.R
@@ -4,234 +4,167 @@
 # Here go all the inputs for the predictor variables
 fluidRow(
   
+  # Move all the input labels to the left
+  tags$head(
+    tags$style(type = "text/css",
+                "label.control-label,
+                .selectize-control.single {
+                  display: table-cell;
+                  text-align: left;
+                  vertical-align : middle;
+                  width: 340px;
+                }
+                .form-group {
+                  display: table-row;
+                }
+                "
+    )
+  ),
+  
   # This is the input area
   
   # Title
-  # This is the column to the left (4 units wide)
-  column(4,
-    style = "background-color: #E8E8E8",     
-         
-    div(
-      style = "display: inline-block;
-              vertical-align:top;
-              text-align:center;
-              width: 100%;",
-      strong("Enter your information below: ")
-    ),
-        
+  # This is the column to the left
+  column(3,
+  
     # Inputs
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Age in years"), 
-      numericInput(
-        inputId = "age1_ext",
-        label   = NULL,
-        value   = 50,
-        min     = 50,
-        max     = 100,
-        step    = 1,
-        width   = 80
-      )
+    numericInput(
+      inputId = "age1_ext",
+      label   = "Your age in years",
+      value   = 50,
+      min     = 50,
+      max     = 100,
+      step    = 1
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Sex"),         
-      selectInput(
-        inputId  = "male_ext",
-        label    = NULL,
-        choices  = c(0, 1), 
-        selected = 0,
-        width    = 80
-      )
+
+    div(style = "margin-top: 6px"),
+    
+    selectInput(
+      inputId  = "male_ext",
+      label    = "Your sex",
+      choices  = c(0, 1), 
+      selected = 0
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Body Mass Index"), 
-      numericInput(
-        inputId = "bmi_ext",
-        label   = NULL,
-        value   = 25,
-        min     = 15,
-        max     = 60,
-        step    = 0.1,
-        width   = 80
-      )
+             
+    numericInput(
+      inputId = "bmi_ext",
+      label   = "Your Body Mass Index (BMI)",
+      value   = 25,
+      min     = 15,
+      max     = 60,
+      step    = 0.1
     ),
-      
-    div(style = "display: inline-block;
-                vertical-align: top;
-                width: 200px;",
-      strong("Diabetes"),
-      selectInput(
-        inputId  = "basediab1_ext",
-        label    = NULL,
-        choices  = c(0, 1),
-        selected = 0,
-        width    = 80
-      )
+
+    div(style = "margin-top: 6px"),
+    
+    selectInput(
+      inputId  = "basediab1_ext",
+      label    = "Do you have diabetes?",
+      choices  = c(0, 1),
+      selected = 0
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Hypertension Medication"),
-      selectInput(
-        inputId  = "drug_hypert_ext",
-        label    = NULL,
-        choices  = c(0, 1),
-        selected = 0,
-        width    = 80
-      )
+    
+    selectInput(
+      inputId  = "drug_hypert_ext",
+      label    = "Do you take ani-hypertension medication?",
+      choices  = c(0, 1),
+      selected = 0
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Daily Smoker"),
-      selectInput(
-        inputId  = "dsmoker_ext",
-        label    = NULL,
-        choices  = c(0, 1),
-        selected = 0,
-        width    = 80
-      )
+    
+    selectInput(
+      inputId  = "dsmoker_ext",
+      label    = "Do you smoke regularly?",
+      choices  = c(0, 1),
+      selected = 0
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Myocardial Infarctiont"),
-      selectInput(
+  
+    selectInput(
         inputId  = "basemi2_ext",
-        label    = NULL,
+        label    = "Have you had myocardial infarction in the past?",
         choices  = c(0, 1),
-        selected = 0,
-        width    = 80
-      )
+        selected = 0
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Alcohol consumption"),
-      numericInput(
-        inputId = "alcave_ext",
-        label   = NULL,
-        value   = 0,
-        min     = 0,
-        max     = 40,
-        step    = 1,
-        width   = 80
-      )
+    
+    numericInput(
+      inputId = "alcave_ext",
+      label   = "Number of drinks you consume daily",
+      value   = 0,
+      min     = 0,
+      max     = 40,
+      step    = 1
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Systolic Blood Pressure"),
-      numericInput(
-        inputId = "systm_ext",
-        label   = NULL,
-        value   = 80,
-        min     = 50,
-        max     = 250,
-        step    = 1,
-        width   = 80
-      )
+  
+    div(style = "margin-top: 6px"),
+    
+    numericInput(
+      inputId = "systm_ext",
+      label   = "Systolic blood pressure",
+      value   = 80,
+      min     = 50,
+      max     = 250,
+      step    = 1
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Diastolic Blood Pressure"),
-      numericInput(
-        inputId = "diastm_ext",
-        label   = NULL,
-        value   = 100,
-        min     = 80,
-        max     = 250,
-        step    = 1,
-        width   = 80
-      )
+    
+    div(style = "margin-top: 6px"),
+    
+    numericInput(
+      inputId = "diastm_ext",
+      label = "Diastolic blood pressure",
+      value = 100,
+      min = 80,
+      max = 250,
+      step = 1
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Levels of NT-proBNP"), 
-      numericInput(
-        inputId = "nt_pro_bnp_ext",
-        label   = NULL,
-        value   = 25,
-        min     = 0,
-        max     = 100,
-        step    = 0.1,
-        width   = 80
-      )
+    
+    div(style = "margin-top: 6px"),
+    
+    numericInput(
+      inputId = "nt_pro_bnp_ext",
+      label   = "Levels of NT-proBNP",
+      value   = 25,
+      min     = 0,
+      max     = 100,
+      step    = 0.1
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Levels of Chola"), 
-      numericInput(
-        inputId = "chola_ext",
-        label   = NULL,
-        value   = 25,
-        min     = 0,
-        max     = 100,
-        step    = 0.1,
-        width   = 80
-      )
+    
+    div(style = "margin-top: 6px"),
+    
+    numericInput(
+      inputId = "chola_ext",
+      label   = "Levels of total cholesterol",
+      value   = 25,
+      min     = 0,
+      max     = 100,
+      step    = 0.1
     ),
-      
-    div(
-      style = "display: inline-block;
-              vertical-align: top;
-              width: 200px;",
-      strong("Levels of EGFR"), 
-      numericInput(
-        inputId = "egfr_ckdepi_crea_ext",
-        label   = NULL,
-        value   = 25,
-        min     = 0,
-        max     = 100,
-        step    = 0.1,
-        width   = 80
-      )
+
+    div(style = "margin-top: 6px"),
+    
+    numericInput(
+      inputId = "egfr_ckdepi_crea_ext",
+      label   = "Levels of eGFR",
+      value   = 25,
+      min     = 0,
+      max     = 100,
+      step    = 0.1
     ),
-      
+    
+    div(style = "margin-top: 6px"),
+    
     # This introduces the actionButton used to run the calculation
     actionButton("enter_ext", label = "Calculate Risk")
-  ), # Close column
+    ), # Close column
   
   # Output area
-  # This is the column to the right (4 units wide)
+
+  # This is the column to the right 
   column(4,
-         # tabsetPanel introduced 
-         tabsetPanel(
-           # Get a summary panel
-           tabPanel("SUMMARY",
-                    textOutput("prediction_ext")
-           ), #Close tabsetPanel
-           # Get the explanation panel
-           tabPanel("EXPLANATION OF THE RESULTS")
-         ) ## Close tabsetPanel
+    # tabsetPanel introduced 
+    tabsetPanel(
+      # Get a summary panel
+      tabPanel("RESULTS",
+        textOutput("prediction_ext")
+      ) # Close tabPanel
+    ) # Close tabsetPanel
   ) # Close column
 ) # Close fluidRow
